22e7d3b3236869b4acc1a80ec873c0596eae9b4f
Jammy12345 <jamwant.1995@gmail.com>
2020-11-14 01:53:20 +0530

    Design Update

--- script.js
+++ script.js
@@ -1,109 +1,71 @@
-
-
-let url = "https://developers.zomato.com/api/v2.1/search?"
+let baseurl = "https://developers.zomato.com/api/v2.1/search?"
 let key = "e7211fb48985ec5e00bfb19c34b6c3ce"
 
-function fetchDetail(){
+
+function getResult(){
     event.preventDefault();
     let form = document.getElementById('form')
     let formData = new FormData(form)
     let q = formData.get('q')
-    let categories = formData.get('categories')
-    let sort = formData.get('sort')
-    let order = formData.get('order')
+    let lat = formData.get('lat')
+    let lon = formData.get('lon')
     let start = formData.get('start')
     let count = formData.get('count')
 
-    let urlsrcparam = new URLSearchParams();
-    
-    urlsrcparam.append('q', q)
-    urlsrcparam.append('categories', categories)
-    urlsrcparam.append('sort', sort)
-    urlsrcparam.append('order', order)
-    urlsrcparam.append('start', start)
-    urlsrcparam.append('count', count)
 
-    //console.log(url + urlsrcparam)
+    let urlSearchParams = new URLSearchParams();
+    
+    
+    urlSearchParams.append('q', q);
+    urlSearchParams.append('lat', lat);
+    urlSearchParams.append('lon', lon);
+    urlSearchParams.append('start', start);
+    urlSearchParams.append('count', count);
+    urlSearchParams.append('q', q);
+    
+    console.log(baseurl + urlSearchParams)
+
 
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", url + urlsrcparam)
+    xhr.open("GET", baseurl + urlSearchParams)
     xhr.setRequestHeader("user-key", key)
     xhr.send()
+
     xhr.onload = function(){
-        var result = JSON.parse(this.response)
-        console.log(result)
-        showDetails(result.restaurants)
+        console.log(this.status)
+        var response = JSON.parse(this.response)
+        console.log(response)
+        displayResponse(response.restaurants)
     }
 
 }
 
 
-function showDetails(data){
-    var show = document.getElementById("show")
-    show.innerHTML = ""
+function displayResponse(data){
+    var res = document.getElementById("res")
+    
+    res.innerHTML = ""
     for(var i = 0; i < data.length; i++){
         var div = document.createElement('div')
-        div.style.border = '2px solid red'
-        div.style.paddingLeft = '10px'
-        div.style.margin = '5px auto'
-        div.style.width = '70%'
-        div.style.borderRadius = '20px'
-
-        var name = document.createElement('p')
-        name.style.fontSize = '30px'
-        name.style.color = 'red'
-        name.style.margin = '5px 10px'
-        name.textContent = "Name:- " + data[i].restaurant.name
-        div.append(name)
-
-        var time = document.createElement('p')
-        time.style.fontSize = '20px'
-        time.style.color = 'green'
-        time.style.margin = '5px 10px'
-        time.textContent = "Timing:- " + data[i].restaurant.timings
-        div.append(time)
-
-        var cost = document.createElement('p')
-        cost.style.fontSize = '20px'
-        cost.style.color = 'teal'
-        cost.style.margin = '5px 10px'
-        cost.textContent = "Avg. Cost for 2:- " + data[i].restaurant.average_cost_for_two
-        div.append(cost)
-
-        var cuisines = document.createElement('p')
-        cuisines.style.fontSize = '20px'
-        cuisines.style.color = 'blue'
-        cuisines.style.margin = '5px 10px'
-        cuisines.textContent = "Cuisines:- " + data[i].restaurant.cuisines
-        div.append(cuisines)
-
-        var phone = document.createElement('p')
-        phone.style.fontSize = '20px'
-        phone.style.color = 'magenta'
-        phone.style.margin = '5px 10px'
-        phone.textContent = "Contact No:- " + data[i].restaurant.phone_numbers
-        div.append(phone)
-
-        var address = document.createElement('p')
-        address.style.fontSize = '20px'
-        address.style.color = 'teal'
-        address.style.margin = '5px 10px'
-        address.textContent = "Address:- " + data[i].restaurant.location.address
-        div.append(address)
-
-        var image = document.createElement('img')
-        image.style.width = '150px'
-        image.style.borderRadius = '10px'
-        image.setAttribute('src', data[i].restaurant.featured_image)
+        console.log(data[i].restaurant.name)
+        var p = document.createElement('p')
+        p.textContent = data[i].restaurant.name
+        div.append(p)
+        var p1 = document.createElement('p')
+        p1.textContent = data[i].restaurant.phone_numbers
+        div.append(p1)
+        var image  = document.createElement('img')
+        image.style.width = '100px'
+        image.setAttribute('src',data[i].restaurant.featured_image)
         div.append(image)
-
-        show.append(div)
+        res.append(div)
     }
+    
+
 }
 
 
-
-window.addEventListener('load', ()=>{
-    var srcbtn = document.querySelector('#srcbtn')
-    srcbtn.addEventListener('click', fetchDetail)
+window.addEventListener('load',()=>{
+    var submitbtn = document.querySelector('#submitbtn')
+    submitbtn.addEventListener('click', getResult)
 })
